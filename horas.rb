e252f8434a1196f362a69e4fc2bb26033d1c9f24
Devix <carlos.joseanguiano@gmail.com>
2017-06-17 12:39:59 -0500

    Horas Dia
    Horas Dia

---
+++ horas.rb
@@ -0,0 +1,10 @@
+una_hora = 59 # 59 Minutos
+hora_dia = 24 # 24 Horas
+un_abo = 365 # 365 dias del ano
+
+puts "Ingresa cuantos dias"
+dia = gets.to_i
+
+    result = (dia * hora_dia)
+
+print "Tus horasB son #{result} !"
